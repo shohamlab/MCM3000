f2c938c67ca4274d3b16608461ba842360b907ae
Stephen Scott Tucker <sstucker@nyu.edu>
2024-05-20 12:35:51 -0400

    homepage

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ name='MCM3000',
 version='0.2',
 author='sstucker',
 author_email='sstucker@nyu.edu',
+homepage='https://github.com/shohamlab/MCM3000',
 description='Python serial interface for Thorlabs MCM3000 Series 3-Axis Controller originally forked from amsikking/thorlabs_MCM3000.',
 packages=['MCM3000'],
 classifiers=[
